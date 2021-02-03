eba55afcc927cbd60a0dd8e58f42cf9a3666cb41
muzta <javyngs@gmail.com>
2021-02-03 18:36:55 +0100

    Solution to day 9 part two

--- AdventOfCode2020/day9/solution.py
+++ AdventOfCode2020/day9/solution.py
@@ -6,7 +6,7 @@ Created on 2 feb. 2021
 file = "input.txt"
 preamble = 25
 
-def ex1(index, lines, listPreambles):
+def partOne(index, lines, listPreambles):
     currentNumber = lines[index]
     canSum = False
     for n in listPreambles:
@@ -18,11 +18,28 @@ def ex1(index, lines, listPreambles):
                 break
         if canSum : break    
     
-    if canSum == False: return lines[index]
+    if canSum == False: return index
     
     newListPreambles = (listPreambles[1:])  # New list without the first number
     newListPreambles.append(currentNumber)   # Add the current number to the preambles
-    return ex1(index+1, lines, newListPreambles)
+    return partOne(index+1, lines, newListPreambles)
+
+
+def partTwo(index, lines, lowerX, lowerY):
+    finalIndex = len(lines) - 1 # The target number to be the result of the sum
+    for x in range(index, finalIndex):
+        for y in range(index + 1, finalIndex):
+            if sum(lines[x:y+1]) == lines[finalIndex]:
+                if (y - x) < (lowerY - lowerX): # If this range is smaller, take it
+                    lowerY = y
+                    lowerX = x
+        return partTwo(index + 1, lines, lowerX, lowerY)
+    
+    #once the range of number is found
+    greatest = max(lines[lowerX:lowerY + 1])
+    lowest = min(lines[lowerX:lowerY + 1])
+    return greatest + lowest
+    
 
 with open(file) as doc:
     lines = doc.readlines()
@@ -30,4 +47,9 @@ lines = [int(x.strip()) for x in lines]
 
 # Part one
 listPreambles = lines[:preamble]
-print("The number which can't be obtained with its preambles is " + str(ex1(preamble, lines, listPreambles)))
+index = partOne(preamble, lines, listPreambles)
+print("The number which can't be obtained with its preambles is " + str(lines[index]))
+
+# Part two
+sumRange = partTwo(0, lines[:index + 1], 0, len(lines))
+print("The sum resulted in part two is " + str(sumRange))
