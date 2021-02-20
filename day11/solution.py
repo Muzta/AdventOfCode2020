f18af1bac9c2b9ee80542c6483616da9caae7ad2
muzta <javyngs@gmail.com>
2021-02-20 16:55:04 +0100

    Revert "Day 11 Part 2"
    This reverts commit fd1a9daee90f19c84dab8d82e6cbf7fa5febf8ee.
    The previous merge commit uploaded again the folder AdventOfCode2020

--- day11/solution.py
+++ day11/solution.py
@@ -10,42 +10,8 @@ file = str(pathlib.Path(__file__).parent) + "\input.txt"
 def partOne(lines):
     newLines = [row[:] for row in lines]    # create a copy of a 2D array
     noMoreChanges = True    # if people has stopped moving
-
-    for r in range(rowSize):
-        for c in range(columnSize):
-
-            nAdjacentSeats = 0
-            actPosition = lines[r][c]   # current position
-            
-            if actPosition != ".":  # if the seat can be occupied
-                
-                for x in range(-1, 2):
-                    for y in range(-1, 2):
-                        if x == y == 0: continue    # it cant be the same position
-                        row = r + x
-                        column = c + y
-                        if (row >= 0 and row < rowSize) and (column >= 0 and column < columnSize) :    # it is in the limit of the input
-                            if lines[row][column] == "#": nAdjacentSeats += 1
-                        
-                if actPosition == "L" and nAdjacentSeats == 0: 
-                    newLines[r][c] = "#"
-                    noMoreChanges = False
-                elif actPosition == "#" and nAdjacentSeats > 3:
-                    newLines[r][c] = "L"
-                    noMoreChanges = False
-                 
-    if noMoreChanges == False: return partOne(newLines)
-    else: return countSeatsOccupied(newLines)
-
-def countSeatsOccupied(lines):
-    count = 0
-    for l in lines:
-        count += l.count("#")
-    return count
-
-def partTwo(lines):
-    newLines = [row[:] for row in lines]    # create a copy of a 2D array
-    noMoreChanges = True    # if people has stopped moving
+    rowSize = len(lines) # number of rows
+    columnSize = len(lines[0]) # number of columns
 
     for r in range(rowSize):
         for c in range(columnSize):
@@ -57,27 +23,24 @@ def partTwo(lines):
                 
                 for x in range(-1, 2):
                     for y in range(-1, 2):
-                        if x == y == 0: continue    # it cant be the same position
-                        row = r
-                        column = c
-                        while True:
-                            row += x
-                            column += y
-                            if (row < 0 or row >= rowSize) or (column < 0 or column >= columnSize): break   # it is not in the limit of the input
-                            if lines[row][column] != ".":  # check the first seat seen
-                                if lines[row][column] == "#": 
-                                    nAdjacentSeats += 1
-                                break
-
+                            row = r + x
+                            column = c + y
+                            if (row >= 0 and row < rowSize) and (column >= 0 and column < columnSize) and not (row == r and column == c) :    # for no checking the same position
+                                if lines[row][column] == "#": nAdjacentSeats += 1
+                        
                 if actPosition == "L" and nAdjacentSeats == 0: 
                     newLines[r][c] = "#"
                     noMoreChanges = False
-                elif actPosition == "#" and nAdjacentSeats > 4:
+                elif actPosition == "#" and nAdjacentSeats > 3:
                     newLines[r][c] = "L"
                     noMoreChanges = False
-         
-    if noMoreChanges == False: return partTwo(newLines)
-    else: return countSeatsOccupied(newLines)
+                 
+    if noMoreChanges == False: return partOne(newLines)
+    else:
+        count = 0
+        for l in newLines:
+            count += l.count("#")
+        return count
 
 
 with open(file) as doc:
@@ -85,11 +48,6 @@ with open(file) as doc:
 lines = [x.strip() for x in lines]
 lines = [list(x) for x in lines]
 
-rowSize = len(lines) # number of rows
-columnSize = len(lines[0]) # number of columns
 
 #Part one
-print("The number of seats occupied in part one is : " + str(partOne(lines)))
-
-#Part two
-print("The number of seats occupied in the second part is : " + str(partTwo(lines)))
+print("The number of seats occupied is : " + str(partOne(lines)))
